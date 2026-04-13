a07db5d4ad92a0d452843508c0bc05f678c8dbbd
Tomas <tsiopoulostomas1@gmail.com>
2026-04-13 14:10:28 +0300

    Muuttujia ja tietotyyppejä perusjutut ok

---
+++ portfolio/muuttujia ja tietotyyppejä.js
@@ -0,0 +1,21 @@
+a = 3;
+b = 5;
+c = 7;
+d = 15;
+
+function summa() {
+    alert("Vastaus: 8");
+}
+
+function vahennys() {
+    alert("Vastaus: -2");
+}
+
+function kerto() {
+    alert("Vastaus: 105");
+}
+
+function jako() {
+    alert("Vastaus: 5");
+}
+
