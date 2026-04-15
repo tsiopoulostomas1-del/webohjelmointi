66d7a35b0aa147a4afee3435d54ed80b91ed60b5
Tomas <tsiopoulostomas1@gmail.com>
2026-04-15 14:12:54 +0300

    teht 22 kesken

--- portfolio/keskustelu.js
+++ portfolio/keskustelu.js
@@ -1,1 +1,11 @@
 console.log("CV-Sivu avattu!") 
+
+function handleClick(e) {
+    e.preventDefault();
+    let email = e.target.form.username.value;
+    console.log(username);
+    let syntyma = e.target.form.syntyma.value;
+    console.log(username)
+    
+}
+
